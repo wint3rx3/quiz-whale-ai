39cae52cfb0a125a9cf91b9c591f5363e308d122
winterx3 <dodamm222@gmail.com>
2025-02-27 18:36:41 +0900

    123

--- server.py
+++ server.py
@@ -1,35 +1,64 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from pathlib import Path
-import fitz  # PyMuPDF
+import magic
 from main import main
 
 app = FastAPI()
 
-UPLOAD_DIR = Path("uploads")
-UPLOAD_DIR.mkdir(parents=True, exist_ok=True)  # 업로드 폴더 생성
+# 업로드 폴더 설정
+BASE_DIR = Path("./")
+UPLOAD_DIRS = {
+    "pdf": BASE_DIR / "PDF",
+    "mp3": BASE_DIR / "MP3"
+}
+
+# 폴더 생성
+for dir_path in UPLOAD_DIRS.values():
+    dir_path.mkdir(parents=True, exist_ok=True)
 
 @app.post("/quizzes")
 async def upload_file(
     file: UploadFile = File(...),
-    type: str = Form(...),  # 파일 타입 (pdf 또는 audio)
+    type: str = Form(...),  # 문제 형식 (CHOICE or SUBJECT)
     start: int = Form(...),
     end: int = Form(...),
     keyword: str = Form(...)
 ):
-    file_path = UPLOAD_DIR / file.filename
+    
+    # Quiz 타입 설정
+    quiz_type = type
+
+    # MIME 타입 확인
+    mime = magic.Magic(mime=True)
+    file_mime_type = mime.from_buffer(await file.read(2048))  # 파일의 처음 2KB로 MIME 검사
+
+    # MIME 타입을 기반으로 파일 타입 결정
+    if file_mime_type == "application/pdf":
+        file_type = "pdf"
+    elif file_mime_type in ["audio/mpeg", "audio/mp3"]:
+        file_type = "mp3"
+    else:
+        return {"error": "Unsupported file type"}
+
+    # 저장할 폴더 선택
+    file_path = UPLOAD_DIRS[file_type] / file.filename
 
     # 파일 저장
     with open(file_path, "wb") as f:
         f.write(await file.read())
 
-    # Main function
-    if type == "pdf" or type == "mp3":
-        json_res = main(type, str(file_path), "json/input.json", "choice", "Not given", 1, 3)
-    else:
-        return {"error": "Invalid file type"}
+    # Main function 실행
+    json_res = main(file_type, str(file_path), "json/input.json", quiz_type, keyword, start, end)
 
-    # 파일 삭제
+    # 처리 후 파일 삭제
     file_path.unlink(missing_ok=True)
 
-    # json 반환
     return json_res
+
+@app.get("/", summary="root EndPoint", description="Check API is Working on Server.")
+async def read_root():
+    return {"name": "quiz_whale"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
